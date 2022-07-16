62b9f6e337ee2352c7568f993c679a43ac4b2913
watnb <watnb153@gmail.com>
2022-07-16 13:29:15 +0900

    Java7月lecture01の課題

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        for (int i = 0; i <15; i++){
+            if (i > 10){
+                System.out.println(i+"回目のHello");
+            }else {
+                System.out.println(i+"回目のHello World");
+            }
+        }
+    }
+}
