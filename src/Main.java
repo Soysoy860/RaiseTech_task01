640a69e537d9a270ef5d8939d98b90e79e2c11df
そい <107778318+Soysoy860@users.noreply.github.com>
2022-07-23 12:11:12 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args){
-        for (int i = 0; i <15; i++){
-            if (i <= 10){
+        for (int i = 0; i <15; i++) {
+            if (i <= 10) {
                 System.out.println(i+"回目のHello World");
-            }else {
+            } else {
                 System.out.println(i+"回目のHello");
             }
         }
